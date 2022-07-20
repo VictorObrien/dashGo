695045b2a596e4d484919fe97ac350abd29f8ed3
Paulo Victor Rebouças <victorobrien@hotmail.com>
2022-07-19 23:22:25 -0300

    feat: finalizing Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,10 @@
-import { Flex, Input, Text, Icon } from "@chakra-ui/react";
-import { RiSearch2Line, RiSearchLine } from "react-icons/ri";
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import {
+  RiNotificationLine,
+  RiSearch2Line,
+  RiSearchLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 
 export function Header() {
   return (
@@ -44,6 +49,36 @@ export function Header() {
           }}
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Paulo Victor</Text>
+            <Text color="gray.300" fontSize="small">
+              pvictor.dev@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Paulo Victor"
+            src="https://github.com/victorobrien.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
